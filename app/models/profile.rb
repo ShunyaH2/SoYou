c3eaea8ea8857f1b609f72030a8a9930aac11536
Shunya Miyake <ks66hawksace@gmail.com>
2025-12-26 22:18:47 +0900

    active storage

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,6 +1,8 @@
 class Profile < ApplicationRecord
   belongs_to :family # 家族に必ず属する
   belongs_to :user, optional: true # 子供等はuser直結でなくてもOK
+
+  has_one_attached :image
   
   # プロフィールは複数投稿と結びつく
   has_many :post_profiles, dependent: :destroy
